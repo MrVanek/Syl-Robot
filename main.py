eac3f3136e610f24d56e766a626bdb1d7af5731f
MatthewVanek <47370185+MrVanek@users.noreply.github.com>
2022-01-29 15:46:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+#  This will be the repo to get the 6 wheeled robot working with Sylvie
+
+
+# Steps:  
+# 1. Get Motor Hat to move a motor
+# 2. Get all motors set up and driving forward and backwards based on code
+# 3. Turning
+# 4. Controller mechanism (Bluetooth game controller or usb?)
