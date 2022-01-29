a0c00fce74f8e67a5916ad83b2da9f8d8ed3cc6a
Matthew Vanek <47370185+MrVanek@users.noreply.github.com>
2022-01-29 16:01:42 +0000

    made robot class and instantiated robot.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from robot import Robot
 #  This will be the repo to get the 6 wheeled robot working with Sylvie
 
 
@@ -6,3 +7,14 @@
 # 2. Get all motors set up and driving forward and backwards based on code
 # 3. Turning
 # 4. Controller mechanism (Bluetooth game controller or usb?)
+
+
+
+# Setup
+jj = Robot()
+
+
+
+
+# Main Program
+pass
---
+++ robot.py
@@ -0,0 +1,19 @@
+from time import sleep
+
+class Robot():
+
+  def __init__(self):
+    print("Initializing robot...")
+
+  def forward(self, amount):
+    sleep(.5)
+    pass
+
+  def backwards(self, amount):
+    pass
+
+  def right(self, amount):
+    pass
+
+  def left(self, amount):
+    pass
