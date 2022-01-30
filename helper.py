6c3bf4a73f99d9563c9a67c2f601372fb78ffad3
Matthew Vanek <47370185+MrVanek@users.noreply.github.com>
2022-01-30 14:53:34 +0000

    bug

--- helper.py
+++ helper.py
@@ -2,7 +2,7 @@ def map_input(x, in_min, in_max, out_min, out_max):
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 def check_deadzone(controller_value, deadzone):
-  if abs(controller_value) < deadzone):
+  if abs(controller_value) < deadzone:
     return 0.0
   else:
     return controller_value 
